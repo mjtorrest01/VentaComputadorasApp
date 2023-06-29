f976a70086ad32725f3430c3f5e772e89596c1f7
mjtorrest <michaeljtt@outlook.com>
2023-06-29 08:06:37 -0500

    Creando Clase Dispositivo de Entrada

---
+++ src/mundopc/modelo/DispositivoEntrada.java
@@ -0,0 +1,34 @@
+package mundopc.modelo;
+
+public class DispositivoEntrada {
+
+    private  String tipoDeEntrada;
+    private String marca;
+
+    public DispositivoEntrada(String tipoDeEntrada, String marca) {
+    }
+
+    public String getTipoDeEntrada() {
+        return tipoDeEntrada;
+    }
+
+    public void setTipoDeEntrada(String tipoDeEntrada) {
+        this.tipoDeEntrada = tipoDeEntrada;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "DispositivoEntrada{" +
+                "tipoDeEntrada='" + tipoDeEntrada + '\'' +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+}
