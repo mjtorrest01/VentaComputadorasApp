cc0b46e22ef78193383edefe24a38b2235cb357e
mjtorrest <michaeljtt@outlook.com>
2023-06-29 10:37:23 -0500

    Quitando get y set

--- src/mundopc/modelo/DispositivoEntrada.java
+++ src/mundopc/modelo/DispositivoEntrada.java
@@ -8,22 +8,6 @@ public class DispositivoEntrada {
     public DispositivoEntrada(String tipoDeEntrada, String marca) {
     }
 
-    public String getTipoDeEntrada() {
-        return tipoDeEntrada;
-    }
-
-    public void setTipoDeEntrada(String tipoDeEntrada) {
-        this.tipoDeEntrada = tipoDeEntrada;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
     @Override
     public String toString() {
         return "DispositivoEntrada{" +
